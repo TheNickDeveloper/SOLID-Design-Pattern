699fada1f75d2c932117ce5597d327ceb5562675
TheNickDeveloper <3235526751@qq.com>
2019-06-07 17:49:47 +0800

    update files

---
+++ DesignPattern/LSP/DemoLibrary/CEO.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class CEO : BaseEmployee, IManage
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 150M;
+
+            Salary = baseAmount * rank;
+        }
+
+        public void GeneratePerformanceReview()
+        {
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+
+        public void FireSomeone()
+        {
+            Console.WriteLine("You're Fired!");
+        }
+    }
+}
---
+++ DesignPattern/LSP/DemoLibrary/Manager.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Manager : BaseEmployee, IManage
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 19.75M;
+
+            Salary = baseAmount + (rank * 4);
+        }
+
+        public void GeneratePerformanceReview()
+        {
+            Console.WriteLine("I'm reviewing a direct report's performance.");
+        }
+    }
+}
